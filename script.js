9f45f4b9ae7bddd817a7eb797b282eba07f03f7c
新川征治 <s2232076gp@s.chibakoudai.jp>
2024-10-31 11:22:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function search() {
     const query = document.getElementById('search-input').value.toLowerCase();
     
     if (query.includes("環七一之江店")) {
-        window.location.href = "green-tea.html";
+        window.location.href = "itinoe.html";
     } else if (query.includes("神田神保町店")) {
-        window.location.href = "black-tea.html";
+        window.location.href = "jinbocho.html";
     } else if (query.includes("野猿街道店２")) {
-        window.location.href = "oolong-tea.html";
+        window.location.href = "yaen.html";
     } else {
         alert("該当する店舗が見つかりませんでした。");
     }
